7453a39293e3907d554528cc00cf894490994fa0
Jerin Easo Regi <jerin.easo.20@gmail.com>
2021-06-15 15:52:09 +0530

    Fully featured todo app

--- src/App.js
+++ src/App.js
@@ -1,66 +1,107 @@
-import {useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import ListTodos from './ListTodos';
 
 function App() {
-  const [todos,setTodos] = useState([]);
+  // Global Todo Array and Added Localstorage data as initial value if not then array will be passed
+  const [todos,setTodos] = useState(()=>{
+    const localData = localStorage.getItem('Todos');
+    return localData ? JSON.parse(localData) : []
+  });
+  // Input Todos
   const [todo,setTodo] = useState('');
 
-  // Date
+  // Date will change on each day
   let newDate = new Date();
   let todayDate = newDate.getDay();
-  let arr = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+  // Created Date
+  let currentTime = `${newDate.getDate()}-${newDate.getMonth()+1}-${newDate.getFullYear()}`
+
+  // Each Days in a week & Dynamically shows each day 
+  const [dateToday, setDateToday] = useState(["Sunday",
+                                              "Monday",
+                                              "Tuesday",
+                                              "Wednesday",
+                                              "Thursday",
+                                              "Friday",
+                                              "Saturday"]);
+
+  // Remaining Items
+  const [todoLeft, setTodoLeft] = useState(0);
   
+  useEffect(() => {
+  // For Remaining items on mounting
+  let totalLeft = todos.filter(item=>!item.status).length;
+  setTodoLeft(totalLeft);
+
+  // Saving the values to storage
+  localStorage.setItem("Todos",JSON.stringify(todos))
+  }, [todos]);
+
   return (
     <div className="app">
-    <div className="mainHeading">
-      <h1>ToDo List</h1>
-    </div>
-    <div className="subHeading">
-      <br />
-      <h2>Whoop, it's  {arr[todayDate]}🌝 ☕ </h2>
-    </div>
-    <div className="input">
-      <input value={todo} onChange={(e)=> setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-      <i onClick={(e)=> setTodos([...todos,{id:Date.now(),text:todo,status:false}])} className="fas fa-plus"></i>
-    </div>
-    <div className="todos">
-      { 
-        todos.map((obj)=>{
-          console.log(todos);
-          return(
-              <div className="todo">
-              <div className="left">
-                <input onChange={
-                  (e)=>{
-                    setTodos(todos.filter(obj2=>{
-                      if(obj2.id === obj.id){
-                        obj2.status=e.target.checked;     
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's  {dateToday[todayDate]}🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input onKeyDown={(e)=>{
+          // Enter Key Function
+          if(e.keyCode=== 13){
+            setTodos([...todos,{id:Date.now(),text:todo,status:false,time:currentTime}]); 
+            setTodo(""); 
+          }
+        }} value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={(e)=>{ 
+          // On Click Function
+          setTodos([...todos,{id:Date.now(),text:todo,status:false,time:currentTime}]); 
+          setTodo(""); }} className="fas fa-plus">
+        </i>
+      </div> 
+      <div className="leftTodos"><p>{todoLeft} items remaining to be done</p></div>
+      <div className="todos">
+        { 
+        // Dynamic to add the input data to be listed
+          todos.map((obj)=>{
+            return(
+                <div className="todo">
+                  
+                <div className="left">
+                  <input onChange={
+                    (e)=>{
+                      setTodos(todos.filter(obj2=>{
+                        if(obj2.id === obj.id){
+                          obj2.status=e.target.checked;     
+                        }
+                        return obj2
+                      }))
                       }
-                      return obj2
-                    }))
-                   }
-                } type="checkbox" name="" id="" />
-                <p className={
-                  (obj.status?"strike":null)
-                }>{obj.text}</p>
+                  } type="checkbox" name="" id="" />
+                  <p className={
+                    (obj.status?"strike":null)
+                  }>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i onClick= {
+                    ()=>{
+                      setTodos(todos.filter(items =>{
+                        return items.id !== obj.id
+                      }))
+                    }
+                  }className="fas fa-times"></i>
+                </div>
               </div>
-              <div className="right">
-                <i onClick= {
-                 ()=>{
-                   setTodos(todos.filter(items =>{
-                     return items.id !== obj.id
-                   }))
-                 }
-                }className="fas fa-times"></i>
-              </div>
-            </div>
-          )
-        })
-      }   
+            )
+          })
+        }   
+      </div>
+    {/* Passing data to another components for next section */}
+    <ListTodos listtodos={todos} />
     </div>
-    <ListTodos listtodos={todos}/>
-  </div>
   );
 }
 
--- src/ListTodos.js
+++ src/ListTodos.js
@@ -1,36 +1,48 @@
 import React from 'react'
 import "./ListTodos.css"
 function ListTodos(props) {
-    return (
-        <div className="list-section">
-            <div className="list-left">
-                <h1 className="headings">In Progress&nbsp;&nbsp;|</h1>
-                {
-                    
-                        props.listtodos.filter((item)=>{
-                         return !item.status 
-                     }).map((value)=>{
-                         return(
-                             <p className="list-text">{value.text}</p>
-                         )
-                     })
-                }
-            </div>
-            <div className="list-right">
-                <h1 className="headings">Completed</h1>
-                {
-                   props.listtodos.filter((item)=>{
-                    return item.status 
-                }).map((value)=>{
-                    return(
-                        <p className="list-text">{value.text}</p>
-                    )
-                })
-                    
-                }
-            </div>
+//    Completed Date
+  let newDate = new Date();
+  let currentTime = `${newDate.getDate()}-${newDate.getMonth()+1}-${newDate.getFullYear()}`
+
+return (
+    <div className="list-section">
+        <div className="main-list">
+        <div className="list-left">
+            <h1 className="headings">In Progress</h1>
+            { 
+            // For Data in Progress
+            props.listtodos.filter((item)=>{
+                return !item.status 
+            }).map((value)=>{
+                return(
+                    <div className="list-text">
+                        <p>{value.text}</p>
+                        <p>Created At : {value.time}</p>
+                    </div>
+                )
+            })
+            }
         </div>
-    )
+        <div className="list-right">
+            <h1 className="headings">Completed</h1>
+            {
+                // For data on completion
+                props.listtodos.filter((item)=>{
+                return item.status 
+            }).map((value)=>{
+                return(
+                    <div className="list-text">
+                    <p>{value.text}</p>
+                    <p>Completed At : {currentTime}</p>
+                    </div>  
+                )
+            })
+            }
+        </div>
+        </div>
+    </div>
+)
 }
 
 export default ListTodos
